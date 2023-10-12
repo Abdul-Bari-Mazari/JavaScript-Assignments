3b29f480a4015c5af9cf5e9f5eac11eb36ab2b58
Abdul Bari Mazari <mazari3339@gmail.com>
2023-10-12 22:19:52 +0500

    Assignment #01 (Complete)

---
+++ #01/app.js
@@ -0,0 +1,117 @@
+// ******************************* Chapter 01 *********************************
+
+//Q1
+//alert("Assalamu Alaikum!!");
+
+//Q2                                   
+// alert("Error! Please enter a valid password.");
+
+//Q3
+// alert("Welcome to JS Land....\nHappy Coding!");
+
+//Q4
+// alert("Welcome to JS Land....");
+// alert("Happy Coding!");
+
+
+//Q5
+//console.log("Hello... I can run JS through my web browser's comsole");
+
+
+// ******************************* Chapter 02 *********************************
+
+
+//Q1
+// var username;
+
+//Q2
+// var myName = "Abdul Bari";
+
+//Q3
+// var message = "Hello World";
+// alert(message);
+
+//Q4
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var education = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age);
+// alert(education);
+
+//Q5
+// alert("Pizza\nPizz\nPiz\nPi\nP");
+
+//Q6
+// var email = "mazari3339@gmail.com";
+// alert("My email address is " +  email);
+
+
+//Q7
+// var book = "A smater way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+//Q8
+// var text = "Yah! I can write HTML content through JS."
+// document.write(text);
+
+
+//Q9
+// var strangeString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(strangeString);
+
+
+
+// ******************************* Chapter 03 *********************************
+
+//Q1
+// var age = 21;
+// alert("I am " + age + " years old");
+
+//Q2
+// var visit = 2;
+// alert("You have visited the website " + visit + " times");
+
+//Q3
+// var birthYear = 2002;
+// document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is number.");
+
+//Q4
+// let name = prompt('Welcome! Enter your name:');
+// let product = prompt('Enter the product you want to buy:');
+// quantity = prompt('Enter quantity:');
+// document.write(name + " ordered " + quantity + " " + product + " on Mazari Clothing Store");
+
+
+
+// ******************************* Chapter 04 *********************************
+
+//Q1
+// name = 'Abdul Bari', age = 21, gender = 'Male';
+
+//Q2
+// var name1, _age, $gender, email, contact; (Legal Variable Names)
+// var alert, prompt, document, 2window, 1write; (Illegal Variable Names)
+
+//Q3
+
+//                              a)
+
+// document.write("<h1>Rules for naming JS variables</h1>")
+
+//                              b)
+
+// document.write("Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable<br>");
+
+//                              c)
+
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name<br>");
+
+//                              d)
+
+// document.write("Variable names are case sensitive.<br>");
+
+//                              e)
+
+// document.write("Variable names should not be JS keywords.<br>");
