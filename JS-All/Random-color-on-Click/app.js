4e399fdb193b3c13907bc4fa249c1d0c28397aac
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-01-06 20:57:54 +0500

    One JavaScript file (Age calculator not done yet)

---
+++ JS-All/Random-color-on-Click/app.js
@@ -0,0 +1,8 @@
+function changeBackColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    document.body.style.background = "rgb("+red+", "+green+", "+blue+")"
+}
+
