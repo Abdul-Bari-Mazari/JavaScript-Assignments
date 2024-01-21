3f46be0193015587f5c18d75ef87d18c315ec85e
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-01-21 11:35:44 +0500

    Years and Months calculated (Not done yet)

--- JS-All/Age-calculator/app.js
+++ JS-All/Age-calculator/app.js
@@ -1,9 +1,9 @@
-// var currentDate = new Date().getTime();
-// var birthDate = new Date("16 Nov 2002").getTime();
+var currentDate = new Date().getTime();
+var birthDate = new Date("16 Nov 2002").getTime();
 
-// var diff = currentDate - birthDate;
+var diff = currentDate - birthDate;
 
-// console.log(diff);
-// console.log(Math.floor(diff / (1000 * 60 * 60 * 24 * 365))); // years
-// console.log(Math.floor(diff / (1000 * 60 * 60 * 24 * 365 * 12))); // Months
-// console.log(Math.floor(diff / (1000 * 60 * 60) % 24) );
+console.log(diff);
+console.log(Math.floor(diff / (1000 * 60 * 60 * 24 * 365))); // years
+console.log(Math.floor(diff / (1000 * 60 * 60 * 24 * 365 * 12))); // Months
+console.log(Math.floor(diff / (1000 * 60 * 60) % 24) );
