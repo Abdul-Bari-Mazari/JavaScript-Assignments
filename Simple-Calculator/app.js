4856f6af15de6c33abcce9279644df45bee451bf
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-01-23 22:48:28 +0500

    Finished!

--- Simple-Calculator/app.js
+++ Simple-Calculator/app.js
@@ -1,8 +1,40 @@
-var buttonValue = document.getElementById("one");
 var input = document.getElementById("input-field");
+var answer = document.getElementById("answer-field");
 
-function getNumber() {
-  input.innerHTML = buttonValue.value;
+function insertChar(inputChar) {
+  input.value += inputChar;
 }
 
-buttonValue.addEventListener('click', getNumber)
+function clearInput() {
+  input.value = "";
+}
+
+function clearAll() {
+  input.value = "";
+  answer.value = "";
+}
+
+function delChar() {
+  input.value = input.value.toString().slice(0, -1);
+}
+
+function square() {
+  input.value = input.value + "*" + input.value;
+}
+
+function upon() {
+  input.value = 1 + "/" + input.value;
+}
+
+function squareRoot() {
+  input.value = Math.sqrt(input.value);
+}
+
+function calulation() {
+  try {
+    answer.value = eval(input.value);
+  } catch (error) {
+    answer.value = "Error";
+  }
+  input.value = "";
+}
