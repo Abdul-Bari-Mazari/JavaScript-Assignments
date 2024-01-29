0df9fd1a497096a541c5cc0ba372d494ab79cd8b
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-01-29 23:28:56 +0500

    Worked on Random Color Generator
    1) Fixed image mouseover and nouseout for Random Color Generator.
    2) Also Added more containers in Random Color Generator

--- JS-All/Random-color-on-Click/app.js
+++ JS-All/Random-color-on-Click/app.js
@@ -1,8 +1,20 @@
 function changeBackColor() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
+    var containerMain = document.getElementById("containerMain");
+    var container4 = document.getElementById("container4");
+    var container3 = document.getElementById("container3");
+    var container2 = document.getElementById("container2");
+    var container1 = document.getElementById("container1");
 
-    document.body.style.background = "rgb("+red+", "+green+", "+blue+")"
+    function getRandomColor() {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        return "rgb(" + red + ","+green+","+blue+")"
+    }
+
+    containerMain.style.backgroundColor = getRandomColor();
+    container4.style.backgroundColor = getRandomColor();
+    container3.style.backgroundColor = getRandomColor();
+    container2.style.backgroundColor = getRandomColor();
+    container1.style.backgroundColor = getRandomColor();
 }
-
--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 function changeImage(newSrc) {
-    var img = document.getElementById('calculatorMain'); // Get the image element by its ID
+    var img = document.getElementById('calculatorMainImage'); // Get the image element by its ID
+    
+    img.style.transition = "all 0.3s ease"; // Set a smooth transition effect for all style changes over 0.3 seconds
 
+    img.style.opacity = 0; // Set the image opacity to 0, making it invisible with a smooth transition
+
+    setTimeout(function () { // Wait for a brief moment to allow the transition effect to be applied
+        img.src = newSrc; // Change the image source to the new source provided
+        img.style.opacity = 1; // Set the image opacity back to 1, making it visible again with a smooth transition
+    }, 100); // Wait for 100 milliseconds (0.1 seconds) before changing the image source and restoring its visibility
+}
+
+function changeColorImage(newSrc) {
+    var img = document.getElementById('colorMainImage'); // Get the image element by its ID
+    
     img.style.transition = "all 0.3s ease"; // Set a smooth transition effect for all style changes over 0.3 seconds
 
     img.style.opacity = 0; // Set the image opacity to 0, making it invisible with a smooth transition
