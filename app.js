0315603a5967d8b11c0c4f764ca11cc60c6da3ed
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-01-30 16:41:49 +0500

    Done with all cards

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 function changeImage(newSrc) {
-    var img = document.getElementById('calculatorMainImage'); // Get the image element by its ID
-    
-    img.style.transition = "all 0.3s ease"; // Set a smooth transition effect for all style changes over 0.3 seconds
+    var img = document.getElementById('calculatorImage');
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0;
 
-    img.style.opacity = 0; // Set the image opacity to 0, making it invisible with a smooth transition
-
-    setTimeout(function () { // Wait for a brief moment to allow the transition effect to be applied
-        img.src = newSrc; // Change the image source to the new source provided
-        img.style.opacity = 1; // Set the image opacity back to 1, making it visible again with a smooth transition
-    }, 100); // Wait for 100 milliseconds (0.1 seconds) before changing the image source and restoring its visibility
+    setTimeout(function () {
+        img.src = newSrc;
+        img.style.opacity = 1;
+    }, 100);
 }
 
 function changeColorImage(newSrc) {
-    var img = document.getElementById('colorMainImage'); // Get the image element by its ID
-    
-    img.style.transition = "all 0.3s ease"; // Set a smooth transition effect for all style changes over 0.3 seconds
+    var img = document.getElementById('colorImage');
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0;
 
-    img.style.opacity = 0; // Set the image opacity to 0, making it invisible with a smooth transition
+    setTimeout(function () {
+        img.src = newSrc;
+        img.style.opacity = 1;
+    }, 100);
+}
 
-    setTimeout(function () { // Wait for a brief moment to allow the transition effect to be applied
-        img.src = newSrc; // Change the image source to the new source provided
-        img.style.opacity = 1; // Set the image opacity back to 1, making it visible again with a smooth transition
-    }, 100); // Wait for 100 milliseconds (0.1 seconds) before changing the image source and restoring its visibility
+function changeRamadanImage(newSrc) {
+    var img = document.getElementById("ramadanImage");
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0
+
+    setTimeout(function () {
+        img.src = newSrc;
+        img.style.opacity = 1
+    }, 100)
 }
 
 
+function changePassImage(newSrc) {
+    var img = document.getElementById("passwordImage");
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0
+
+    setTimeout(function() {
+        img.src = newSrc;
+        img.style.opacity = 1;
+    }, 100)
+}
+
+function changeAgeImage(newSrc) {
+    var img = document.getElementById("ageImage");
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0
+
+    setTimeout(function() {
+        img.src = newSrc;
+        img.style.opacity = 1;
+    }, 100)
+}
+
+function changeFbImage(newSrc) {
+    var img = document.getElementById("fbImage");
+    img.style.transition = "all 0.3s ease";
+    img.style.opacity = 0
+
+    setTimeout(function() {
+        img.src = newSrc;
+        img.style.opacity = 1;
+    }, 100)
+}
