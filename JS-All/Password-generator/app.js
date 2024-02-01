b448112fd16cbd7bb0b159d579a98ab9e1322b88
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-02-01 13:03:14 +0500

    Added functionality to generate password with checkboxes and length

--- JS-All/Password-generator/app.js
+++ JS-All/Password-generator/app.js
@@ -1,25 +1,96 @@
-const rangeInput = document.getElementById("input");
-const sliderNumber = document.getElementById("range-text");
-const generateField = document.getElementById("generate-field");
-const generateButton = document.getElementById("generate-button");
+// Generation IDs
+var rangeInput = document.getElementById("input");
+var sliderNumber = document.getElementById("range-text");
+var output = document.getElementById("output");
+var generateButton = document.getElementById("generate-button");
 
-var allChars =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ1234567890!@#$%^&*()_+;:<>?/";
+var error = document.getElementById("error");
+
+// Checkbox IDs
+var smallCheckbox = document.getElementById("small-letter-checkbox");
+var capitalCheckbox = document.getElementById("capital-letter-checkbox");
+var numberCheckbox = document.getElementById("number-checkbox");
+var specialCheckbox = document.getElementById("special-char-checkbox");
+
+// Characters
+var smallLetters = "abcdefghijklmnopqrstuvwxyz";
+var capitalLetters = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+var numbers = "123456789012345678901234567890";
+var specialCharacter = "!@#$%^&*()_+;:<>?/!@#^$&#*#()*&^%%$#";
 
 // Generate Password function
-const generatePass = () => {
+
+function generatePass() {
   var newPassword = "";
+  var selectedChars = "";
 
-  for (let i = 0; i < rangeInput.value; i++) {
-    var randomNumbers = Math.floor(Math.random() * allChars.length);
-    newPassword += allChars[randomNumbers];
+  if(smallCheckbox.checked) {
+    selectedChars += smallLetters;
   }
-  generateField.value = newPassword;
-};
 
-rangeInput.addEventListener("input", () => {
-  sliderNumber.innerText = rangeInput.value;
-  generatePass();
-});
+  if(capitalCheckbox.checked) {
+    selectedChars += capitalLetters;
+  }
 
-generateButton.addEventListener("click", generatePass);
+  if(numberCheckbox.checked) {
+    selectedChars += numbers;
+  }
+
+  if(specialCheckbox.checked) {
+    selectedChars += specialCharacter;
+  }
+
+
+  console.log(selectedChars, smallLetters.length);
+
+  for(let i = 0; i < rangeInput.value; i++) {
+    var randomNumbers = Math.floor(Math.random() * selectedChars.length);
+    console.log(selectedChars[randomNumbers])
+    newPassword += selectedChars[randomNumbers];
+
+  output.value = newPassword;
+  error.innerHTML = "";
+
+  if(smallCheckbox.checked == false && capitalCheckbox.checked == false && numberCheckbox.checked == false && specialCheckbox.checked == false) {
+    error.innerHTML = "(Select any one to include)";
+    error.style.color = "red";
+    error.style.fontWeight = "light";
+    output.value = "";
+    }
+
+  }
+
+}
+
+function rangeTextToValue() {
+  sliderNumber.innerHTML = rangeInput.value;
+}
+
+
+
+
+// **** Tried different logic ****
+
+// if(smallCheckbox.checked == true) { // Small letters
+//   newPassword += smallLetters[randomNumbers];
+// }
+
+// else if(smallCheckbox.checked == true && capitalCheckbox.checked == true) { // Small and capital letters
+//   newPassword += (smallLetters[randomNumbers] + capitalLetters[randomNumbers]);
+// }
+
+// else if(smallCheckbox.checked == true && numberCheckbox.checked == true) { // Small letters and numbers
+//   newPassword += (smallLetters[randomNumbers] + numbers[randomNumbers]);
+// }
+
+// else if(capitalCheckbox.checked == true && numberCheckbox.checked == true) { // Capital letters and numbers
+//   newPassword += (capitalLetters[randomNumbers] + numbers[randomNumbers]);
+// }
+
+// else if(capitalCheckbox.checked == true && numberCheckbox.checked == true && smallCheckbox == true) { // Capital, small letters and numbers
+//   newPassword += (capitalLetters[randomNumbers] + numbers[randomNumbers] + smallLetters[randomNumbers]);
+// }
+
+// else if(capitalCheckbox.checked == true && numberCheckbox.checked == true && smallCheckbox == true && specialCheckbox == true) { // Capital, small letters, numbers and special chars
+//   newPassword += (capitalLetters[randomNumbers] + numbers[randomNumbers] + smallLetters[randomNumbers] + specialCharacter[randomNumbers]);
+// }
